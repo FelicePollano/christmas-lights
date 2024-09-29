3b1927096b8895c3370094fa0d7cadf1ddbef1a7
Felice Pollano <felice@felicepollano.com>
2024-09-29 11:36:26 +0200

    added two more modes

---
+++ src/christmas-lights/main/alt.h
@@ -0,0 +1,5 @@
+int alt_init(uint8_t *data,int count,RGB_t (*cpicker)()); //init function
+int alt_run(uint8_t *data,int count,RGB_t (*cpicker)()); //run function
+int alt_run2(uint8_t *data,int count,RGB_t (*cpicker)());
+int alt_run3(uint8_t *data,int count,RGB_t (*cpicker)());
+void alt_clean(uint8_t *data ,int count); //cleanup function
