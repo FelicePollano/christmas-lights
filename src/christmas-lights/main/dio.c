0d005bac37f5789f2e1876b8650fe8eaaf572493
Felice Pollano <felice@felicepollano.com>
2024-11-05 08:45:50 +0100

    added push button for remaining commands

--- src/christmas-lights/main/dio.c
+++ src/christmas-lights/main/dio.c
@@ -5,21 +5,29 @@
 
 #include "cl.h"
 
-//according to the docs this pin supports internal pull-up resistor ( and pull up is even the default at wakeup )
+//according to the docs these pin supports internal pull-up resistor ( and pull up is even the default at wakeup )
 //I want mode chang button to act when grounded, in order to avoid having 3v3 volt extra wire to handle positive logic 
 #define MODENEXT 5 
-#define CCHANGE 9
+#define MODEPREV 7
+#define CNEXT 9
+#define CPREV 11
 // handle digital I/O
 void dio(void* ctx){
     Context_t *context=(Context_t*)ctx;
     int mchanged=0;
+    int xmchanged=0;
     int cchanged=0;
+    int xcchanged=0;
     int current_mode=0;
     int current_color=0;
     ESP_ERROR_CHECK(gpio_set_direction(MODENEXT,GPIO_MODE_INPUT));
     ESP_ERROR_CHECK(gpio_set_pull_mode(MODENEXT,GPIO_PULLUP_ONLY));
-    ESP_ERROR_CHECK(gpio_set_direction(CCHANGE,GPIO_MODE_INPUT));
-    ESP_ERROR_CHECK(gpio_set_pull_mode(CCHANGE,GPIO_PULLUP_ONLY));
+    ESP_ERROR_CHECK(gpio_set_direction(MODEPREV,GPIO_MODE_INPUT));
+    ESP_ERROR_CHECK(gpio_set_pull_mode(MODEPREV,GPIO_PULLUP_ONLY));
+    ESP_ERROR_CHECK(gpio_set_direction(CNEXT,GPIO_MODE_INPUT));
+    ESP_ERROR_CHECK(gpio_set_pull_mode(CNEXT,GPIO_PULLUP_ONLY));
+    ESP_ERROR_CHECK(gpio_set_direction(CPREV,GPIO_MODE_INPUT));
+    ESP_ERROR_CHECK(gpio_set_pull_mode(CPREV,GPIO_PULLUP_ONLY));
     while(1){
         int pin = gpio_get_level(MODENEXT);
         if(pin){
@@ -35,7 +43,21 @@ void dio(void* ctx){
                  context->mode=&context->modes[current_mode];
             }
         }
-        pin = gpio_get_level(CCHANGE);
+        pin = gpio_get_level(MODEPREV);
+        if(pin){
+            xmchanged=0;
+        }else{
+            if(!xmchanged){
+                 xmchanged=1;
+                 if(current_mode>0){
+                    current_mode--;
+                 }else{
+                    current_mode=context->modes_count-1;
+                 }
+                 context->mode=&context->modes[current_mode];
+            }
+        }
+        pin = gpio_get_level(CNEXT);
         if(pin){
             cchanged=0;
         }else{
@@ -45,6 +67,20 @@ void dio(void* ctx){
                     current_color++;
                  }else{
                     current_color=0;
+                 }
+                 context->cpicker=context->cpickers[current_color];
+            }
+        }
+        pin = gpio_get_level(CPREV);
+        if(pin){
+            xcchanged=0;
+        }else{
+            if(!xcchanged){
+                 xcchanged=1;
+                 if(current_color>0){
+                    current_color--;
+                 }else{
+                    current_color=context->cpickers_count-1;
                  }
                  context->cpicker=context->cpickers[current_color];
             }
